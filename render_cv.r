d2d21f30c4d781edf5a622eda7f8bdfbe78f70f4
Eleanor Jackson <eleanor.elizabeth.j@gmail.com>
2022-01-25 16:40:55 +0000

    add resume rendering

--- render_cv.r
+++ render_cv.r
@@ -5,6 +5,8 @@
 # loaded and load the cached version in the .Rmd instead of re-fetching it twice
 # for the HTML and PDF rendering. This exercise is left to the reader.
 
+# CV ----------------------------------------------------------------------
+
 # Knit the HTML version
 rmarkdown::render("cv.rmd",
                   params = list(pdf_mode = FALSE),
@@ -19,3 +21,20 @@ rmarkdown::render("cv.rmd",
 # Convert to PDF using Pagedown
 pagedown::chrome_print(input = tmp_html_cv_loc,
                        output = "cv.pdf")
+
+# Resume ------------------------------------------------------------------
+
+# Knit the HTML version
+rmarkdown::render("resume.rmd",
+                  params = list(pdf_mode = FALSE),
+                  output_file = "resume.html")
+
+# Knit the PDF version to temporary html location
+tmp_html_cv_loc <- fs::file_temp(ext = ".html")
+rmarkdown::render("resume.rmd",
+                  params = list(pdf_mode = TRUE),
+                  output_file = tmp_html_cv_loc)
+
+# Convert to PDF using Pagedown
+pagedown::chrome_print(input = tmp_html_cv_loc,
+                       output = "resume.pdf")
